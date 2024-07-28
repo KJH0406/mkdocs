9664629c179fe69b9240b58855dfbde4e4ed58b5
김장호 <rlawkdgh0406@naver.com>
2024-07-28 15:22:33 +0900

    feat: 초기 메시지 구성 및 스타일 변경

--- manual/overrides/chatbot.js
+++ manual/overrides/chatbot.js
@@ -1,3 +1,5 @@
+const api = "http://127.0.0.1:8001"
+
 class Chatbot {
   constructor() {
     this.toggleBtn = document.getElementById("chatbot-toggle")
@@ -13,24 +15,41 @@ class Chatbot {
     this.userInput.addEventListener("keypress", (e) => {
       if (e.key === "Enter") this.sendMessage()
     })
+
+    this.initializeButtons()
   }
 
   toggleChat() {
     this.chatWindow.classList.toggle("hidden")
   }
 
-  async sendMessage() {
-    const message = this.userInput.value.trim()
+  initializeButtons() {
+    const buttons = this.chatMessages.querySelectorAll(".service-button")
+    buttons.forEach((button) => {
+      button.addEventListener("click", () =>
+        this.sendMessage(button.textContent)
+      )
+    })
+  }
+
+  addMessage(sender, message) {
+    const messageElement = document.createElement("div")
+    messageElement.classList.add("message", `${sender}-message`)
+    messageElement.textContent = message
+    this.chatMessages.appendChild(messageElement)
+    this.chatMessages.scrollTop = this.chatMessages.scrollHeight
+  }
+
+  async sendMessage(message = this.userInput.value.trim()) {
     if (message === "") return
 
     this.addMessage("user", message)
     this.userInput.value = ""
 
-    // 로딩 인디케이터 추가
     const loadingIndicator = this.addLoadingIndicator()
 
     try {
-      const response = await fetch("http://127.0.0.1:8001/chat", {
+      const response = await fetch(`${api}/chat`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -44,18 +63,9 @@ class Chatbot {
       console.error("Error:", error)
       this.updateMessage(
         loadingIndicator,
-        "Sorry, an error occurred. Please try again."
+        "오류가 발생했습니다. 다시 시도해주세요."
       )
     }
-  }
-
-  addMessage(sender, message) {
-    const messageElement = document.createElement("div")
-    messageElement.classList.add("message", `${sender}-message`)
-    messageElement.textContent = message
-    this.chatMessages.appendChild(messageElement)
-    this.chatMessages.scrollTop = this.chatMessages.scrollHeight
-    return messageElement
   }
 
   addLoadingIndicator() {
@@ -72,11 +82,8 @@ class Chatbot {
 
   updateMessage(element, newMessage) {
     element.classList.remove("loading-message")
-    element.innerHTML = newMessage // 로딩 인디케이터를 메시지로 교체
+    element.innerHTML = newMessage
   }
 }
 
-// 페이지 로드 시 Chatbot 인스턴스 생성
-document.addEventListener("DOMContentLoaded", () => {
-  new Chatbot()
-})
+document.addEventListener("DOMContentLoaded", () => new Chatbot())
